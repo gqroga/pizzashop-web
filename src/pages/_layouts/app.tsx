3f23f37abb1f0cf73e71520d2e39a360fc23dfde
gqroga <gcqueirogat@gmail.com>
2025-03-02 23:46:19 -0300

    feature: layout do app com cabeçalho

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components/header'
+
 export function AppLayout() {
   return (
-    <div>
-      <h1>Cabeçalho</h1>
+    <div className="fixed top-0 left-0 w-full h-screen">
+      <Header />
 
-      <div>
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
