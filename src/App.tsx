c42e366179e5d5d20a83dc146495c68e6c9b00ac
gqroga <gcqueirogat@gmail.com>
2025-03-02 21:44:00 -0300

    feat: login através de Magic Link Authentication e atualizações no layout da página

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import { RouterProvider } from 'react-router-dom'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { router } from './routes'
+import { Toaster } from 'sonner'
+
 
 export default function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop " />
+
+      <Toaster richColors />
+
         <RouterProvider router={router} />
     </HelmetProvider>
   )
-
 }
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,7 +3,8 @@ import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
+
+    <div className="grid grid-cols-2 fixed top-0 left-0 w-full h-screen">
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
@@ -21,4 +22,3 @@ export function AuthLayout() {
     </div>
   )
 }
-
